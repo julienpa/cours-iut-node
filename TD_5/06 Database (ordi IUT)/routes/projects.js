f7d70574350eb1f7e34f4d9472bd0d22a4581724
Alex C <seafoox@gmail.com>
2018-02-18 08:54:20 -0800

    Fixed bug and removed useless ;

--- TD_5/06 Database (ordi IUT)/routes/projects.js
+++ TD_5/06 Database (ordi IUT)/routes/projects.js
@@ -1,21 +1,22 @@
-"use strict";
+"use strict"
 
 const express = require('express')
 const pg = require('pg')
 const fs = require('fs')
-const router = express.Router();
+const router = express.Router()
 
-const conString = "postgres://awwonxhh:NaLo34BWjG_BPxCnIjrTd8A2gde64tO9@baasu.db.elephantsql.com:5432/awwonxhh";
+const conString = "postgres://awwonxhh:NaLo34BWjG_BPxCnIjrTd8A2gde64tO9@baasu.db.elephantsql.com:5432/awwonxhh"
 
 router.get('/', (req, res) => {
-  executeQuery("SELECT * FROM projects;", [], (result) => {
+  executeQuery("SELECT * FROM projects", [], (result) => {
     res.render('projects', { projectsList: result.rows })
   })
 })
 
 router.get('/:id', (req, res) => {
-  executeQuery("SELECT * FROM projects WHERE id = $1;", [req.params.id], (result) => {
-    res.render('project_details', { project: result.rows[0] })
+  executeQuery("SELECT * FROM projects WHERE id = $1", [req.params.id], (result) => {
+    let details = result.rows[0]
+    res.render('project_details', { project: details })
   })
 })
 
@@ -23,15 +24,15 @@ function executeQuery(sql, params, callback) {
   // 1. Connection
   pg.connect(conString, function(err, client, done) {
     if(err) {
-      return console.error('error fetching client from pool', err);
+      return console.error('error fetching client from pool', err)
     }
     // 2. Execute the query
     client.query(sql, params, function(err, result) {
       // 3. Close Connection
-      done();
+      done()
 
       if (err) {
-        console.log(err);
+        console.log(err)
       }
       else {
         // 4. Execute the callback(res)
--- TD_5/06_Database (ordi perso)/routes/projects.js
+++ TD_5/06_Database (ordi perso)/routes/projects.js
@@ -1,24 +1,22 @@
-"use strict";
+"use strict"
 
 const express = require('express')
 const fs = require('fs')
-const router = express.Router();
+const router = express.Router()
 const { Client } = require('pg')
 
-const conString = "postgres://waktnwfi:ceIrQsu630rgaKfLZYLX1iEUJG-W43-b@horton.elephantsql.com:5432/waktnwfi";
-
 router.get('/', (req, res) => {
-   executeQuery("SELECT * FROM projects;", [], (result) => {
+   executeQuery("SELECT * FROM projects", [], (result) => {
     res.render("projects", {projects_list: result.rows})
    })
-});
+})
 
 router.get('/:id', (req, res) => {
-    executeQuery("SELECT * FROM projects WHERE id=$1;", [req.params.id], (results) => {
-        let details = results.rows[0];
-        res.render('project_details', { project: result.rows[0] })
+    executeQuery("SELECT * FROM projects WHERE id=$1", [req.params.id], (result) => {
+        let details = result.rows[0]
+        res.render('project_details', { project: details })
     })
-});
+})
 
 function executeQuery(sql, params, callback){
     // 1. Connection
@@ -28,22 +26,22 @@ function executeQuery(sql, params, callback){
         database: 'waktnwfi',
         password: 'ceIrQsu630rgaKfLZYLX1iEUJG-W43-b',
         port: 5432,
-    });
-    client.connect();
+    })
+    client.connect()
 
     // 2. Execute the query
     client.query(sql, params, (err, dbRes) => {
         // 3. Close Connection
-        client.end();
+        client.end()
 
         if (err) {
-            console.log(err);
+            console.log(err)
         }
         else {
             // 4. Execute the callback(res)
-            callback(dbRes);
+            callback(dbRes)
         }
-    });
+    })
 }
 
-module.exports = router;
+module.exports = router
